cf21d3a106b217fde82dc0e0311926e6e96f0ba6
“galizien“ <galizien@users.noreply.github.com>
2021-01-11 19:47:35 +0200

    finished 8 sprint

---
+++ SPRINT 8. JSX, Re-render, Styles. SCRUM theory/task-3-style/src/components/app/app.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import './app.css'
+import Button from '../button/button.js';
+
+export default function App() {
+    return <div className="wrapper-app" data-testid="element-app"><Button /><div>gos</div></div>
+}
---
+++ SPRINT 8. JSX, Re-render, Styles. SCRUM theory/task-3-style/src/components/button/button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Button() {
+    return <button style={{
+        color: 'rgb(18, 255, 18)',
+        backgroundColor: 'transparent',
+        border: '0px',
+        fontFamily: 'Courier',
+        frontSize: '33pt',
+    }} data-testid="element-button"> Wake up Neo...</button >;
+}
